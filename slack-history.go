f3f7745cd1e0436ee56d30ad5afd1c8e92c7ad64
Lei Guo <lei.guo@hootsuite.com>
2019-05-25 06:54:20 -0700

    add basic wordcloud support

--- slack-history.go
+++ slack-history.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/bbalet/stopwords"
+	"github.com/cdipaolo/goml/base"
+	"github.com/cdipaolo/goml/text"
 	"github.com/nlopes/slack"
 )
+
+var text2Analyze string
+var outputFile = "./wordcloud.png"
+var inputFile = "./text2analyze.txt"
+var stopwordsFile = "./stopwords.txt"
 
 func sanitizeNewLines(str string) string {
 	noNewLine := strings.Replace(str, "\n", `\n`, -1)
@@ -19,25 +29,27 @@ func sanitizeNewLines(str string) string {
 	return noNewLine
 }
 
-func writeHistory(history *slack.History, csvWriter *csv.Writer, onlyBots bool) error {
+func writeHistory(history *slack.History, csvWriter *csv.Writer, bots bool, includeStopWords bool) error {
 	//log.Printf("%#v\n", history.Messages[0:5])
 	for _, m := range history.Messages {
-		if onlyBots && m.BotID != "" {
+		if !bots && m.BotID != "" {
 			continue
 		}
 
 		text := m.Text
-		if m.File != nil {
+		if m.Files != nil {
 			//log.Printf("%#v", m.File)
 			//out, err := ioutil.ReadAll(*m.File)
 			//if err != nil {
 			//	log.Fatalf("Failed to read attached file. %s", err)
 			//}
 
-			text += `\n`
-			text += m.File.Name
-			text += `\n`
-			text += sanitizeNewLines(m.File.Preview)
+			for _, file := range m.Files {
+				text += `\n`
+				text += file.Name
+				text += `\n`
+				text += sanitizeNewLines(file.Preview)
+			}
 		}
 
 		if len(m.Attachments) > 0 {
@@ -51,21 +63,131 @@ func writeHistory(history *slack.History, csvWriter *csv.Writer, onlyBots bool) 
 		if err != nil {
 			log.Printf("timestamp %s not valid, cannot parse. Err: %s", m.Timestamp, err)
 		}
-		// ignore milliseconds for now
+		//ignore milliseconds for now
 		intTs := int64(floatTs)
 		goTime := time.Unix(intTs, 0)
 
 		text = strings.Replace(text, "\n", "\\n", -1)
 
-		err = csvWriter.Write([]string{goTime.Format(time.RFC3339), m.User, text})
+		var cleanText string
+
+		if !includeStopWords {
+			cleanText = stopwords.CleanString(text, "en", true)
+		} else {
+			cleanText = text
+		}
+
+		err = csvWriter.Write([]string{goTime.Format(time.RFC3339), m.User, cleanText})
+		text2Analyze += cleanText + " "
+
 		if err != nil {
-			log.Printf(`Failed to write "%s"`, text)
+			log.Printf(`Failed to write "%s"`, cleanText)
 		}
 		csvWriter.Flush()
-		//fmt.Println(m.Timestamp, m.Username, m.Text)
 
 	}
 	return nil
+}
+
+func textClassification() {
+	// create the channel of data and errors
+	stream := make(chan base.TextDatapoint, 100)
+	errors := make(chan error)
+
+	// make a new NaiveBayes model with
+	// 2 classes expected (classes in
+	// datapoints will now expect {0,1}.
+	// in general, given n as the classes
+	// variable, the model will expect
+	// datapoint classes in {0,...,n-1})
+	//
+	// Note that the model is filtering
+	// the text to omit anything except
+	// words and numbers (and spaces
+	// obviously)
+	model := text.NewNaiveBayes(stream, 2, base.OnlyWordsAndNumbers)
+
+	go model.OnlineLearn(errors)
+
+	stream <- base.TextDatapoint{
+		X: "I love the city",
+		Y: 1,
+	}
+
+	stream <- base.TextDatapoint{
+		X: "I hate Los Angeles",
+		Y: 0,
+	}
+
+	stream <- base.TextDatapoint{
+		X: "My mother is not a nice lady",
+		Y: 0,
+	}
+
+	close(stream)
+
+	for {
+		err, _ := <-errors
+		if err != nil {
+			fmt.Printf("Error passed: %v", err)
+		} else {
+			// training is done!
+			break
+		}
+	}
+
+	// now you can predict like normal
+	//fmt.Printf("Text to analyze: %#v\n", text2Analyze)
+
+	// class := model.Predict(text2Analyze) // 0
+
+	// fmt.Printf("Prediction: %v", class)
+	// tfidf := text.TFIDF(*model)
+	// fmt.Printf("Term frequency: %v", tfidf)
+
+	wordCloud()
+}
+func createFile() {
+	// detect if file exists
+	var _, err = os.Stat(inputFile)
+
+	// create file if not exists
+	if !os.IsNotExist(err) {
+		e := os.Remove(inputFile)
+		if isError(e) {
+			return
+		}
+
+		fmt.Println("==> done deleting file")
+	}
+	var file, e = os.Create(inputFile)
+	if isError(e) {
+		return
+	}
+	defer file.Close()
+	fmt.Println("==> done creating file", inputFile)
+}
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return err != nil
+}
+
+func wordCloud() {
+
+	createFile()
+	f, _ := os.OpenFile(outputFile, os.O_APPEND|os.O_RDWR, 0644)
+	writer := bufio.NewWriter(f)
+	fmt.Fprintln(writer, text2Analyze)
+	writer.Flush()
+	defer f.Close()
+
+	cmd := exec.Command("kumo", "--input", "./output.csv", "--output", outputFile, "--word-count", "200", "--stop-words", "related,come,team,server,things,run,work,right,added,channel,wondering,bin,bunch,occured,you're,day,mean,recently,output,slightly_smiling_face,looks,pod,ok,okay,going,help,just,try,ticket,issue,service,look,thanks,need,use,like,sure,new,good,it's,want,having,don't,i'll,did,able,morning,actually,th,adding,smile,fine,default,eyes,i've,using,getting,set,check,thank,didn,problem,set,know,trying,think,looking,running,sorry,needs,info,guys,maybe,bit,that's,point,yeah,doesn't,anymore,does,tried,used,needed,weird,better,lot,correct,can't,ago,write,hi,create,following,working,file,created,hootsuite,works,i'm,stuff,type,checking,thing")
+	cmd.CombinedOutput()
+
 }
 
 func main() {
@@ -73,7 +195,9 @@ func main() {
 	end := flag.String("end", "", "End Time in ISO8601 (default is current time)")
 	channel := flag.String("channel", "devops", "Channel Name to get logs for")
 	filePath := flag.String("write", "output.csv", "where to output the file")
-	bots := flag.Bool("bots", false, "only print bot messages")
+	bots := flag.Bool("bots", false, "include bot messages")
+	stopwords := flag.Bool("stopwords", false, "include stop words")
+
 	flag.Parse()
 	token := os.Getenv("SLACK_TOKEN")
 	if token == "" {
@@ -88,12 +212,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to get channels. %s", err)
 	}
-	channelID := ""
+	channelID := "C65PEKN9J"
 	for _, ch := range channels {
 		if ch.Name == *channel {
 			channelID = ch.ID
 		}
-		//fmt.Printf("Channel: %#v\n", channel)
+		//fmt.Printf("Channel: %#v\n", ch.Name)
 	}
 
 	if channelID == "" {
@@ -142,7 +266,7 @@ func main() {
 	//fmt.Println("latest", endTimeTs)
 
 	//log.Println(len(history.Messages))
-	writeHistory(history, csvWriter, *bots)
+	writeHistory(history, csvWriter, *bots, *stopwords)
 	lastMessageTs := history.Messages[len(history.Messages)-1].Timestamp
 	log.Println("lastMessageTs", lastMessageTs)
 	if history.HasMore {
@@ -153,7 +277,7 @@ func main() {
 			if err != nil {
 				log.Fatalf("Failed to get history for channel. %s", err)
 			}
-			writeHistory(history, csvWriter, *bots)
+			writeHistory(history, csvWriter, *bots, *stopwords)
 			lastMessageTs = history.Messages[len(history.Messages)-1].Timestamp
 			//log.Printf("history %+v", history.Messages)
 			//log.Println("lastMessageTs", lastMessageTs)
@@ -165,6 +289,6 @@ func main() {
 	}
 
 	fmt.Println("Wrote", *filePath)
-
+	textClassification()
 	//fmt.Printf("%#v\n", history)
 }
